cafbab99168428c763d7b259b82c567ccbf067ed
Jeshua Hicks <jhicks7884@outlook.com>
2019-10-21 16:15:41 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,7 +30,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-matrix, [1], [1]
+([ENFIELD_TENNIS_ACADEMY]), [1], [1]
     # Return the matrix's content at row and column
 end
 
